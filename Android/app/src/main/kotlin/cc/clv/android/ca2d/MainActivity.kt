ffe002f7d02fa5895e65908790c9f62d843699b7
slightair <arksutite@gmail.com>
2016-03-17 22:49:44 +0900

    Add drawer

--- Android/app/src/main/kotlin/cc/clv/android/ca2d/MainActivity.kt
+++ Android/app/src/main/kotlin/cc/clv/android/ca2d/MainActivity.kt
@@ -2,10 +2,18 @@ package cc.clv.android.ca2d
 
 import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import cc.clv.android.ca2d.graphics.Renderer
 
 class MainActivity : AppCompatActivity() {
+    lateinit private var drawerLayout: DrawerLayout
+    lateinit private var drawerList: ListView
+    lateinit private var drawerToggle: ActionBarDrawerToggle
     lateinit private var worldView: GLSurfaceView
     lateinit private var renderer: Renderer
 
@@ -14,11 +22,29 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        drawerLayout = findViewById(R.id.drawer_layout) as DrawerLayout
+        drawerList = findViewById(R.id.left_drawer) as ListView
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        drawerList.adapter = ArrayAdapter<String>(this, R.layout.drawer_list_item, Rule.presets.map { it.name })
+
         worldView = findViewById(R.id.fullscreen_content) as GLSurfaceView
         worldView.setEGLContextClientVersion(2)
 
         renderer = Renderer(applicationContext)
         worldView.setRenderer(renderer)
         worldView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
